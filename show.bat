afd6de8749bf9b00360371fad4db695f3fb820b6
gusta7w7 <githubabelha7w7@gmail.com>
2025-07-24 17:27:13 +0000

    Upload New File

---
+++ show.bat
@@ -0,0 +1,16 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+
+echo Telegram: https://t.me/TheDisala4U
+echo Created by a Brazilian
+echo By abelha7w7
+echo Method Fixed 2025!!!
+
+echo User name : runneradmin
+echo User Pass : TheDisa1a
+
+echo Chupa Gitlab de Merda!
